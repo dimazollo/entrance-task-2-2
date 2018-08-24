81273030468b54d17b429505fa4bc6d8167a4c89
Dmitry Volovod <dvolovod@it.ru>
2018-08-24 23:38:13 +0300

    add device control window generator function

--- src/js/app.js
+++ src/js/app.js
@@ -362,3 +362,70 @@ function fillPageWithHouseItems (
     }
   }
 }
+
+function generateDeviceControl(container, data) {
+  var template = `
+    <div class="popup-container__content device-controller">
+      <div class="popup-container__header popup-header">
+        <div class="popup-header__text-description text-description">
+          <div class="text-description__name">${data.name}</div>
+          <div class="text-description__info">${data.description}</div>
+        </div>
+        <div class="popup-header__status device-status">
+          <div class="device-status__current-value">
+            ${data.currentValue}
+          </div>
+          <img class="device-status__icon" 
+               src="../assets/${data.icon}@2x.png">
+        </div>
+      </div>
+      ${data.control === 'slider-light' ?
+        '<div class="device-controller__menu">' + 
+          '<ul class="mode-menu">' +
+            '<li class="mode-menu__button mode-menu__button_highlighted">Вручную</li>' +
+            '<li class="mode-menu__button">Дневной свет</li>' +
+            '<li class="mode-menu__button">Вечерный свет</li>' +
+            '<li class="mode-menu__button">Рассвет</li>' +
+          '</ul>' +
+        '</div>' : '' }
+      ${data.control === 'slider-light' ?
+        '<div class="device-controller__menu">' +
+          '<ul class="mode-menu">' +
+            '<li class="mode-menu__button mode-menu__button_highlighted">Вручную</li>' +
+            '<li class="mode-menu__button">Холодно</li>' +
+            '<li class="mode-menu__button">Тепло</li>' +
+            '<li class="mode-menu__button">Жарко</li>' +
+          '</ul>' +
+        '</div>' : '' }
+      <div class="device-controller__controls">
+          ${data.control === 'knob' ?
+            '<div><input type="text" value="' +
+                data.currentValue + '" ' +
+                'class="controls__knob"></div>': ''}
+          ${data.control === 'slider-temperature' ? 
+            '<input type="range" min="-10" max="30" value="' +
+                data.currentValue + 
+                '" class="controls__slider ' +
+                'controls__slider_type_temperature">' : ''}
+          ${data.control === 'slider-light' ?
+            '<input type="range" min="1" max="100" value="' +
+                data.currentValue + '" ' +
+                'class="controls__slider ' +
+                'controls__slider_type_light">' : ''}
+      </div>
+    </div>
+    `
+
+  var newHtmlElement = document.createElement('div')
+  newHtmlElement.innerHTML = template
+  container.insertBefore(newHtmlElement, container.firstChild)
+}
+
+function updateScrollButtonsForFeaturedDevices () {
+  var element = document.querySelector('.devices-pane')
+  if (element.scrollWidth > element.offsetWidth) {
+    $('#featured-devices .pagination').show(100)
+  } else {
+    $('#featured-devices .pagination').hide(100)
+  }
+}
